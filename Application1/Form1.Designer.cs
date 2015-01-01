49f7abf93569261d676ee25fa50e8f767cd8b0e9
Rizwan <rizawnbashir@lumensoft.biz>
2015-01-01 11:44:09 +0500

    11002 f

--- Application1/Form1.Designer.cs
+++ Application1/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -141,11 +142,21 @@
             this.label7.TabIndex = 0;
             this.label7.Text = "11002 e";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(148, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "11002 f";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.linkLabel2);
             this.Controls.Add(this.label6);
@@ -177,6 +188,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.LinkLabel linkLabel2;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
     }
 }
 
